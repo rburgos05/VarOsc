1832d217d7a1d54a3d1baec9c1527500710a8b6d
rburgos05 <rburgos05@hotmail.com>
2016-09-06 23:51:03 -0500

    Adding -Q in port 12, plus On/Off resonance frequency
    Enabling port 12 to output the negated signal (Q')of port 11 (Q).
    Adding functionality to turn the oscillator on and off.

--- Variable_Oscillator/Variable_Oscillator.ino
+++ Variable_Oscillator/Variable_Oscillator.ino
@@ -1,62 +1,88 @@
-#define ledPin 13
+#define resonanceFreqPin    13
+#define negResonanceFreqPin 12
 #define CLKIO 16000000           // System Clock frequency
-#define PreScalerMask0 0xF8      // Pre-Scaler divider = 0 (Timer/Counter stop)
+#define PreScalerMask0 0xF8      // Pre-Scaler divider = 0 (Timer/Counter0 stop)
+#define PreScalerMask1 0xF8      // Pre-Scaler divider = 0 (Timer/Counter1 stop)
 
 unsigned int preScalerOptions[] = {0, 1, 64, 1024};
+boolean resonanceFreqIsOn = true;
+unsigned long microsInterval = 4000000;   // Default to 4 seconds
+unsigned long previousMicros = 0;
+
+void setOnOFFResonanceFreq(float onOffResonanceFreq)
+{
+  if (onOffResonanceFreq > 0)
+  {
+    microsInterval = 1000000 / onOffResonanceFreq;  // Convert frequency to microseconds
+  }
+  else
+  {
+    Serial.println("Invalid on/off frequency. No changes have been done.");
+  }
+}
 
 void setup() {
   Serial.begin(9600);
-  pinMode(ledPin, OUTPUT);
-  
-  // initialize timer1 
-  noInterrupts();           // disable all interrupts
+  pinMode(resonanceFreqPin, OUTPUT);
+  pinMode(negResonanceFreqPin, OUTPUT);
+
+  digitalWrite(resonanceFreqPin, LOW);   // Start with the LED pin turned off
+  digitalWrite(negResonanceFreqPin, HIGH);   // Opposite value of LED pin
+
+  noInterrupts();           // Disable all interrupts before initilizing timers
+
+  // Initialize timer1
   TCCR1A = 0;
   TCCR1B = 0;
   TCNT1  = 0;
 
-  OCR1A = 7812;                   // compare match register 16MHz /(1024 * 1Hz) / 2
+  // Compare match register 16MHz / 1024 / Desired Freq / 2
+  OCR1A = CLKIO / preScalerOptions[3] / 1 / 2;  // Default resonace frequeny 1 Hz
+  
   TCCR1B |= (1 << WGM12);         // CTC mode
   TCCR1B |= (1 << CS12) | (1 << CS10);   // Set Pre-Scaler divider to 1024
   TIMSK1 |= (1 << OCIE1A);        // enable timer compare interrupt
-  interrupts();                   // enable all interrupts
-  Serial.print("Frequency set to 1.0000 Hz");
+  
+  interrupts();   // Timers initialization done, enable all interrupts
+
+  setOnOFFResonanceFreq(0.25);
+  Serial.print("On/off frequency set to "); Serial.print(0.25, 4); Serial.println(" Hz");
+  Serial.print("Resonance frequency set to 1.0000 Hz");
   Serial.print("     OCR1A = "); Serial.println(OCR1A);
 
 /* Debug line */
 //  Serial.print("            TCCR1B: "); Serial.println(TCCR1B, BIN);
 }
 
-void setFreq (float freq)
+void setFreq (float freq, byte ocrSize)
 {
   unsigned int preScalerDivider = preScalerOptions[0];
   
-  if (freq < (CLKIO / preScalerOptions[3] / pow(2,16))) {   // Requested frequency is too slow
+  if (freq < (CLKIO / preScalerOptions[3] / pow(2, ocrSize * 8))) {   // Requested frequency is too slow
     Serial.println("");
     Serial.print("Frequency must be greater than ");
-    Serial.print(CLKIO / preScalerOptions[3] / pow(2,16), 4);
+    Serial.print(CLKIO / preScalerOptions[3] / pow(2, ocrSize * 8), 4);
     Serial.println(" Hz.");
-    Serial.printf("The timer has been stopped.\nEnter a new value to restart the timer.\n");
-    TCCR1B &= PreScalerMask0;   // Set the Clock Select to No Clock (Timer/Counter stopped)
-    digitalWrite(ledPin, LOW);
+    Serial.println("No changes have been done.");
   }
-  else if (freq < (CLKIO / pow(2,6) / pow(2,16))) {   // Very low frequency
+  else if (freq < (CLKIO / pow(2,6) / pow(2, ocrSize * 8))) {   // Very low frequency
     preScalerDivider = preScalerOptions[3];
-    TCCR1B = (TCCR1B & PreScalerMask0) | 1 << CS12 | 1 << CS10;   // Set Pre-Scaler divider to 1024
+    TCCR1B = (TCCR1B & PreScalerMask1) | 1 << CS12 | 1 << CS10;   // Set Pre-Scaler divider to 1024
   }
-  else if (freq < (CLKIO / pow(2,16))) {   // Low frequency
+  else if (freq < (CLKIO / pow(2, ocrSize * 8))) {   // Low frequency
     preScalerDivider = preScalerOptions[2];
-    TCCR1B = (TCCR1B & PreScalerMask0) | 1 << CS11 | 1 << CS10;   // Set Pre-Scaler divider to 64
+    TCCR1B = (TCCR1B & PreScalerMask1) | 1 << CS11 | 1 << CS10;   // Set Pre-Scaler divider to 64
   }
   else if (freq <= 36363) {   // Maximum supported frequency
     preScalerDivider = preScalerOptions[1];
-    TCCR1B = (TCCR1B & PreScalerMask0) | 1 << CS10;   // Set Pre-Scaler divider to 1 (practically no divider)
+    TCCR1B = (TCCR1B & PreScalerMask1) | 1 << CS10;   // Set Pre-Scaler divider to 1 (practically no divider)
   }
   else {
-    Serial.printf("\nFrequency can be maximum 36363.0000 Hz.");
-    Serial.printf("\nThe timer has been stopped.");
-    Serial.printf("\nEnter a new value to restart the timer.\n");
-    TCCR1B &= PreScalerMask0;   // Set the Clock Select to No Clock (Timer/Counter stopped)
-    digitalWrite(ledPin, LOW);
+    Serial.println(""); Serial.print("Frequency can be maximum 36363.0000 Hz.");
+    Serial.println(""); Serial.print("The timer has been stopped.");
+    Serial.println(""); Serial.println("Enter a new value to restart the timer.");
+    TCCR1B &= PreScalerMask1;   // Set the Clock Select to No Clock (Timer/Counter stopped)
+    digitalWrite(resonanceFreqPin, LOW);
   }
 
   if (preScalerDivider > 0) {
@@ -71,20 +97,59 @@ void setFreq (float freq)
 
 ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
 {
-  digitalWrite(ledPin, digitalRead(ledPin) ^ 1);   // toggle LED pin
+  if (resonanceFreqIsOn)
+  {
+    digitalWrite(resonanceFreqPin, digitalRead(resonanceFreqPin) ^ 1);   // Toggle LED pin
+    digitalWrite(negResonanceFreqPin, digitalRead(resonanceFreqPin) ^ 1);   // Opposite value of LED pin
+  }
+  else
+  {
+    // The resonance frequency output is off, don't change anything on the output.
+  }
 }
 
+void toggleOnOffResonanceFreq()
+{
+  if ((micros() - previousMicros) >= microsInterval)
+  {
+    previousMicros = micros();
+    resonanceFreqIsOn = !resonanceFreqIsOn;
+
+    if (!resonanceFreqIsOn)
+    {
+      // The resonance frequency has been disabled, turn off the LED pin
+      digitalWrite(resonanceFreqPin, LOW);
+      digitalWrite(negResonanceFreqPin, HIGH);   // Opposite value of the LED pin
+    }
+  }
+  else
+  {
+    // Nothing to do, it's still not time to turn on/off the resonance frequency
+  }
+}
 
 void loop() {
-  while (Serial.available() > 0) {
-    // Read the desired frequency
-    float freq = Serial.parseFloat();
-    if (freq > 0) {
-//      Serial.println("");
-      Serial.print("Frequency set to "); Serial.print(freq, 4); Serial.print(" Hz");
-      setFreq(freq);
+  while (Serial.available() > 0)
+  {
+    if (Serial.peek() == 'a')
+    {
+      // Read the desired on/off frequency
+      float freq = Serial.parseFloat();
+      
+      setOnOFFResonanceFreq(freq);
+      Serial.print("On/off frequency set to "); Serial.print(freq, 4); Serial.println(" Hz");
+    }
+    else
+    {
+      // Read the desired resonance frequency
+      float freq = Serial.parseFloat();
+      
+      setFreq(freq, sizeof(OCR1A));
+      Serial.print("Resonance frequency set to "); Serial.print(freq, 4); Serial.print(" Hz");
       Serial.print("     OCR1A = "); Serial.println(OCR1A);
     }
   }
+
+  toggleOnOffResonanceFreq();
 }
 
